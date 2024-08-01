83f7905d2dc39a1a9b110d9e21fed54fc0e21491
rwmob <rich.moberg@gmail.com>
2024-08-01 10:39:01 -0400

    Update streamlit_app.py
    corrected session

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ name_on_order = st.text_input("Name On Smoothie:")
 st.write("The name on your smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
-session = cnx.get_active_session()
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)

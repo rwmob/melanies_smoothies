cd5f2a06647d299be82a8c15f72cda3caa349fc8
rwmob <rich.moberg@gmail.com>
2024-08-01 11:05:59 -0400

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,11 +36,11 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)

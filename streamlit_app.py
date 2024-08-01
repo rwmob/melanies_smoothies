cad9fd783855a0d0d59cd3efb223e4041d2e845a
rwmob <rich.moberg@gmail.com>
2024-08-01 11:04:22 -0400

    Update streamlit_app.py
    Add stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,10 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
